59fa302b82226dbfb2884cdd3ccc2646dc48875f
aspdotnetdevloper <myselfharshsharma@gmail.com>
2024-12-06 19:36:09 +0530

    dynamic dropdown binding.

--- src/FormModuleDemo/FormModuleDemo.component.ts
+++ src/FormModuleDemo/FormModuleDemo.component.ts
@@ -1,12 +1,21 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector :'app-form-demo',
     templateUrl : './FormModuleDemo.html',
-    imports : [FormsModule]
+    imports : [FormsModule,CommonModule]
 })
 
 export class FormModuleDemoComponent{
-    Name ='harsh'
+    JobTitles = [
+        { value: 1, label : 'Manager'},
+        { value: 2, label : 'Programmer'},
+        { value: 3, label : 'QA'},
+        { value: 4, label : 'Director'},
+    ];
+    Name ='harsh';
+    Id =2;
+    
 }
